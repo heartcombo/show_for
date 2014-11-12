689999755264404a679f21e5e7b0df1df93b22b2
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2014-11-12 09:58:12 -0200

    Ability to generate configuration for Twitter Bootstrap.

--- lib/generators/show_for/install_generator.rb
+++ lib/generators/show_for/install_generator.rb
@@ -3,10 +3,13 @@ module ShowFor
     class InstallGenerator < Rails::Generators::Base
       desc "Copy ShowFor installation files"
       class_option :template_engine, :desc => 'Template engine to be invoked (erb or haml or slim).'
+      class_option :bootstrap, :type => :boolean, :default => false,
+        :desc => 'Generate config for boostrap, using dl, dt and dd tags.'
       source_root File.expand_path('../templates', __FILE__)
 
       def copy_initializers
-        copy_file 'show_for.rb', 'config/initializers/show_for.rb'
+        copy_file "show_for#{options.bootstrap? && '_bootstrap' || nil}.rb",
+                  'config/initializers/show_for.rb'
       end
 
       def copy_locale_file
